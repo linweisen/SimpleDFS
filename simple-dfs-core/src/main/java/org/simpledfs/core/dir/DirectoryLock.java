90c6ff20758b7acd6c7484bf4ecf089b5329bdb3
linweisen <linws.sai@gmail.com>
2021-12-08 17:49:32 +0800

    修改为读写锁

--- simple-dfs-core/src/main/java/org/simpledfs/core/dir/DirectoryLock.java
+++ simple-dfs-core/src/main/java/org/simpledfs/core/dir/DirectoryLock.java
@@ -5,11 +5,13 @@ import org.simpledfs.core.exception.NonDirectoryException;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class DirectoryLock {
 
-    private Map<String, Lock> lockMap;
+    private Map<String, ReadWriteLock> lockMap;
 
     private DirectoryLock(){
         lockMap = new ConcurrentHashMap<>();
@@ -20,26 +22,26 @@ public class DirectoryLock {
     }
 
     public void lock(String directory){
-        Lock lock = lockMap.get(directory);
+        ReadWriteLock lock = lockMap.get(directory);
         if (lock == null){
             throw new NonDirectoryException();
         }
-        lock.lock();
+        lock.readLock();
     }
 
     public void unlock(String directory){
-        Lock lock = lockMap.get(directory);
+        ReadWriteLock lock = lockMap.get(directory);
         if (lock == null){
             throw new NonDirectoryException();
         }
-        lock.unlock();
+        lock.writeLock().unlock();
     }
 
     public void addLock(String directory){
         if (lockMap.containsKey(directory)){
             throw new NonDirectoryException();
         }
-        lockMap.put(directory, new ReentrantLock());
+        lockMap.put(directory, new ReentrantReadWriteLock());
     }
 
     public void removeLock(String directory){
